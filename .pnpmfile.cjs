6ef8cef0426a5d9c2879685f41df685f306d01d6
Arun George <aruniverse@users.noreply.github.com>
2022-02-04 10:24:35 -0500

    add cra

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,10 +1,8 @@
+// A hack only to be used when packages have incorrect dependencies
 function readPackage(pkg) {
-  if (pkg.name == "@itwin/imodels-access-backend") {
-    pkg.dependencies["@itwin/core-backend"] = "workspace:*";
-    pkg.dependencies["@itwin/core-bentley"] = "workspace:*";
-    pkg.dependencies["@itwin/core-common"] = "workspace:*";
-    pkg.dependencies["@itwin/core-geometry"] = "workspace:*";
-  }
+  // if (pkg.name == "@itwin/foo") {
+  //   pkg.dependencies["@itwin/bar"] = "workspace:*";
+  // }
   return pkg;
 }
 
